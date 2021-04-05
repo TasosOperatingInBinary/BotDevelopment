e9129b85e8e3a9339d9d9e292f8af8db86b949f9
Anastasios Papadopoulos <apapadoi@csd.auth.gr>
2021-04-05 18:15:04 +0300

    Integrated calview command with db.

--- src/main/java/org/acm/auth/commands/CalendarViewCommand.java
+++ src/main/java/org/acm/auth/commands/CalendarViewCommand.java
@@ -2,15 +2,25 @@ package org.acm.auth.commands;
 
 import com.google.api.services.calendar.Calendar;
 import com.vdurmont.emoji.EmojiParser;
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.acm.auth.db.Tables;
 import org.acm.auth.services.GoogleCalendar;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jooq.*;
+import org.jooq.exception.DataAccessException;
+import org.jooq.exception.NoDataFoundException;
+import org.jooq.impl.DSL;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 import java.awt.*;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.SQLTimeoutException;
 
 /**
  * Represents a command that displays the description of the calendar that corresponds to the guild
@@ -18,7 +28,10 @@ import java.io.IOException;
  */
 public class CalendarViewCommand extends Command{
     private static final Logger LOGGER = LogManager.getLogger(CalendarViewCommand.class);
-    private static final String CALENDAR_ID = "tln0hmfdrimk3e3j3ngaj7vvag@group.calendar.google.com";
+    private static final Dotenv env = Dotenv.load();
+    private static final String url = "jdbc:mariadb://localhost:3306";
+    private static final String username = env.get("MYSQL_USER");
+    private static final String password = env.get("MYSQL_PASSWORD");
 
     /**
      * Constructs a CalendarView command
@@ -34,9 +47,34 @@ public class CalendarViewCommand extends Command{
 
     @Override
     public void invoke(MessageReceivedEvent event, String[] args) {
+        if(event.getAuthor().isBot())
+            return;
+
         if(!GoogleCalendar.utilityObjectsInitialized(event, args))
             return;
 
+        Record calendarRecord;
+        try {
+            calendarRecord = fetchCalendarId(event,args);
+
+        } catch (NoDataFoundException e) { // the query returned no rows
+            event.getChannel().sendMessage("There is no calendar associated with the current server! Please set the " +
+                    "server's calendar with the @@@@ command and then try again executing this command. "
+                    + EmojiParser.parseToUnicode(":blush:"))
+                    .queue();
+            return;
+
+        } catch (SQLException|DataAccessException e) {  // something went wrong while executing a SQL statement from jOOQ ( DataAccessException )
+                                                        // a database access error occurred or the url is null ( SQLException )
+            LOGGER.error(e.getMessage());
+            event.getChannel().sendMessage("There is an error with our database. Please try again later. " +
+                    EmojiParser.parseToUnicode(":innocent:"))
+                    .queue();
+            return;
+        }
+
+        // the query returned successfully the calendar id
+        String CALENDAR_ID= (String)calendarRecord.get(1);
         Calendar service = GoogleCalendar.getCalendarService();
 
         try {
@@ -87,5 +125,20 @@ public class CalendarViewCommand extends Command{
                 .addField(new MessageEmbed.Field("Integrate our calendar with your Google Calendar:",
                         calendar.getId(),true,true))
                 .build();
+    }
+
+    /**
+     * Returns the calendar id of the guild the command was executed.
+     * @return the record of the table CALENDARS as {@link org.jooq.Record}
+     * @throws SQLException if a database access error occurred or the url is null
+     * @throws DataAccessException if something went wrong while executing a SQL statement from jOOQ
+     */
+    private Record fetchCalendarId(MessageReceivedEvent event, String[] args) throws SQLException, DataAccessException{
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+            DSLContext create = DSL.using(conn, SQLDialect.MARIADB);
+
+            // fetch guild's corresponding calendar
+            return create.fetchSingle(Tables.CALENDARS, Tables.CALENDARS.GUILD_ID.eq(event.getGuild().getId()));
+        }
     }
 }
